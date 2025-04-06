ca09f08b1ee71b62806e68002a00543b11a4354c
Vithushana <shanavithu16@gmail.com>
2025-04-07 00:09:53 +0530

    navbar add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<LoginPage />} />
