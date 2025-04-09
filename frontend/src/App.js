c7f43aec5664c5e3232e907058e255aa46dfa078
Vithushana <shanavithu16@gmail.com>
2025-04-09 18:24:29 +0530

    register-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import LoginPage from './pages/LoginPage';
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage/>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+import "../styles/LoginPage.css"; // Reuse same styling
+import background from "../images/login.jpg"; // Same background
+
+function RegisterPage() {
+  return (
+    <div className="login-container">
+      <div className="left-side" style={{ backgroundImage: `url(${background})` }}>
+        <div className="text-content">
+          <span className="template-tag">Template</span>
+          <h1>Start your journey<br />with <span>eLearning</span></h1>
+        </div>
+      </div>
+
+      <div className="right-side">
+        <div className="form-box">
+          <h2>Create your account</h2>
+          <form>
+            <div className="input-group">
+              <FaUserAlt className="input-icon" />
+              <input type="text" placeholder="Full Name" />
+            </div>
+            <div className="input-group">
+              <MdEmail className="input-icon" />
+              <input type="email" placeholder="Your e-mail" />
+            </div>
+            <div className="input-group">
+              <RiLockPasswordLine className="input-icon" />
+              <input type="password" placeholder="Password" />
+            </div>
+
+            <div className="form-options">
+              <Link to="/login">Already have an account?</Link>
+            </div>
+
+            <button type="submit" className="login-btn">Register</button>
+          </form>
+
+          <div className="social-login">
+            <p>Sign up with</p>
+            <Link to="/register-facebook">facebook</Link>
+            <Link to="/register-google">google</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterPage;
